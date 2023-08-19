55800344c32c6023c9af23c395f40fa5d0a585d1
sjad0005 <sjad0005@student.monash.edu>
2023-08-20 04:26:29 +1000

    Delete done

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ app.get("/category/33306036/add", function(req,res) {
 
 app.post("/category/33306036/add", function (req,res) {
     let reqBody = req.body;
-    console.log(reqBody);
     let currentDate = new Date;
     let aCategory = new Category(reqBody.categoryID, reqBody.name, reqBody.description, reqBody.image, currentDate);
     categoriesDB.push(aCategory);
@@ -68,7 +67,7 @@ app.get("/category/33306036/show-event-details", function (req,res) {
     res.render("show-event-details", {events: eventsDB});
 });
 
-//filtering category list by keyword in name and descrip
+//filtering category list by keyword in name and description
 app.get("/category/33306036/list-by-keyword" , function(req, res) {
     const keyword = req.query.keyword;
     const filteredCategories = categoriesDB.filter(function(category) {
@@ -97,7 +96,7 @@ app.get("/category/33306036/delete-by-ID", function (req, res) {
 
 app.post("/category/33306036/delete-by-ID", function (req,res) {
     // let categoryID =parseInt(req.body.categoryID); 
-   const categoryID = req.body.categoryID;
+    const categoryID = req.body.categoryID;
     for (let i = 0; i < categoriesDB.length; i++) {
 		console.log("Comparing with category:", categoriesDB[i].id);
         if (categoriesDB[i].id === categoryID) {
@@ -114,13 +113,17 @@ app.post("/category/33306036/delete-by-ID", function (req,res) {
 
 //Sidd
 const Event = require("./models/event");
+const { log } = require('console');
 
 
 
 //Adding a new event
 app.get("/sidd/events/add", function(req, res){
-    res.sendFile( VIEWS_PATH + "add-event.html" )
-  
+    if (categoriesDB.length == 0){
+        res.sendFile( VIEWS_PATH + "add-event-without-category.html" )
+    } else{
+        res.sendFile( VIEWS_PATH + "add-event.html" )
+    }
 })
 
 app.post("/sidd/events/add", function(req, res){
@@ -135,11 +138,31 @@ app.get("/sidd/events", function(req, res){
     res.render("list-all-events", { events: eventsDB });
 })
 
-
-// Listing sold out events
+//Listing sold out events
 app.get("/sidd/soldout-events", function(req, res){
     res.render("list-soldout-events", { events: eventsDB });
 })
+
+//Deleting an event
+app.get("/sidd/events/delete", function(req, res){
+    const ID = req.query.id;
+    
+    if(ID==undefined){
+        res.sendFile( VIEWS_PATH + "delete-event.html" )
+    }else{
+        // console.log(ID)
+        // console.log(eventsDB[0]);
+        for (let i = 0; i < eventsDB.length; i++) {
+            if (eventsDB[i].ID === ID) {
+                categoriesDB.splice(i, 1);
+                break;
+            } 
+        }
+
+        res.redirect("/sidd/events");
+    }
+})
+    
 
 
 //404 errors
