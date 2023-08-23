ce54a9c20846320880e77fd7f1eae5a4cb2fed49
sjad0005 <sjad0005@student.monash.edu>
2023-08-23 20:14:44 +1000

    Added routing

--- routes/event.js
+++ routes/event.js
@@ -1,88 +1,93 @@
-// let express = require("express");
-// let router = express.Router();
-// const morgan = require("morgan")
-// const ejs = require("ejs");
-// let path = require("path");
+let express = require("express");
+let router = express.Router();
+const morgan = require("morgan")
+const ejs = require("ejs");
+let path = require("path");
 
-// const { VIEWS_PATH } = require("../server");
+const categoryRouter = require("./event-category");
+// express.use("/" , categoryRouter);
 
-// pathRoot = "/Users/siddharthjadli/Monash/2023Sem2/FIT2095/Ass/assignment-1/server.js";
+pathRoot = "/Users/siddharthjadli/Monash/2023Sem2/FIT2095/Ass/assignment-1/server.js";
 
-// const app = express();
+// const router = express();
 
-// app.use(express.static("node_modules/bootstrap/dist/css"));
-// app.use(morgan('tiny'));
+router.use(express.static("node_modules/bootstrap/dist/css"));
+router.use(morgan('tiny'));
 
-// app.use(express.urlencoded({ extended: true}));
-// app.use(express.json());
+router.use(express.urlencoded({ extended: true}));
+router.use(express.json());
 
-// app.use(express.static("images"));
-// app.engine("html", ejs.renderFile);
-// app.set("view engine", "html");
+// router.use(express.static("images"));
+// router.engine("html", ejs.renderFile);
+// router.set("view engine", "html");
 
-// const Event = require("../models/event");
+const Event = require("../models/event");
+let eventsDB = [];
 
-// //Adding a new event
-// app.get("/sidd/events/add", function(req, res){
-//     if (categoriesDB.length == 0){
-//         res.sendFile( VIEWS_PATH + "add-event-without-category.html" );
-//     } else{
-//         res.sendFile( VIEWS_PATH + "add-event.html" );
-//     }
-// })
+//Adding a new event
+router.get("/sidd/events/add", function(req, res){
+    // if (categoriesDB.length == 0){
+    //     res.render("add-event-without-category" );
+    // } else{
+    //     res.render("add-event" );
+    // }
+    res.render("add-event" );
+})
 
-// app.post("/sidd/events/add", function(req, res){
-//     let reqBody = req.body;
-//     let newEvent = new Event(reqBody.name, reqBody.description, reqBody.startDateTime, reqBody.duration, reqBody.isActive, reqBody.image, reqBody.capacity, reqBody.ticketsAvailable, reqBody.categoryID);
-//     eventsDB.push(newEvent);
-//     res.redirect("/sidd/events");
-// })
+router.post("/sidd/events/add", function(req, res){
+    let reqBody = req.body;
+    let newEvent = new Event(reqBody.name, reqBody.description, reqBody.startDateTime, reqBody.duration, reqBody.isActive, reqBody.image, reqBody.capacity, reqBody.ticketsAvailable, reqBody.categoryID);
+    eventsDB.push(newEvent);
+    res.redirect("/sidd/events");
+})
 
-// //Listing all events
-// app.get("/sidd/events", function(req, res){
-//     res.render("list-all-events", { events: eventsDB });
-// })
+//Listing all events
+router.get("/sidd/events", function(req, res){
+    res.render("list-all-events", { events: eventsDB });
+})
 
-// // Listing sold out events
-// app.get("/sidd/soldout-events", function(req, res){
-//     res.render("list-soldout-events", { events: eventsDB });
-// })
+// Listing sold out events
+router.get("/sidd/soldout-events", function(req, res){
+    res.render("list-soldout-events", { events: eventsDB });
+})
 
-// // Show category detail
-// app.get("/sidd/category", function(req, res){
-//     const showCategoryId = req.query.id;
+// Show category detail
+router.get("/sidd/category", function(req, res){
+    const showCategoryId = req.query.id;
     
-//     if(categoriesDB.length == 0){
-//         res.sendFile( VIEWS_PATH + "show-category-without-categories.html" );
-//     }else{
-//         if(showCategoryId==undefined){
-//             res.render("show-category-details", { categories: categoriesDB, index: 0 , events: eventsDB});
-//         }else{
-//             for (let i = 0; i < categoriesDB.length; i++) {
-//                 if (categoriesDB[i].id == showCategoryId) {
-//                     res.render("show-category-details", { categories: categoriesDB, index: i , events: eventsDB});
-//                     break;
-//                 } 
-//             }
-//         }
-//     }
-// })
+    if(categoriesDB.length == 0){
+        res.render("show-category-without-categories.html" );
+    }else{
+        if(showCategoryId==undefined){
+            res.render("show-category-details", { categories: categoriesDB, index: 0 , events: eventsDB});
+        }else{
+            for (let i = 0; i < categoriesDB.length; i++) {
+                if (categoriesDB[i].id == showCategoryId) {
+                    res.render("show-category-details", { categories: categoriesDB, index: i , events: eventsDB});
+                    break;
+                } 
+            }
+        }
+    }
+})
 
 
-// //Deleting an event
-// app.get("/sidd/events/delete", function(req, res){
-//     const deleteid = req.query.id;
+//Deleting an event
+router.get("/sidd/events/delete", function(req, res){
+    const deleteid = req.query.id;
     
-//     if(deleteid==undefined){
-//         res.sendFile( VIEWS_PATH + "delete-event.html" )
-//     }else{
-//         for (let i = 0; i < eventsDB.length; i++) {
-//             if (eventsDB[i].id === deleteid) {
-//                 eventsDB.splice(i, 1);
-//                 break;
-//             } 
-//         }
+    if(deleteid==undefined){
+        res.sendFile( VIEWS_PATH + "delete-event.html" )
+    }else{
+        for (let i = 0; i < eventsDB.length; i++) {
+            if (eventsDB[i].id === deleteid) {
+                eventsDB.splice(i, 1);
+                break;
+            } 
+        }
 
-//         res.redirect("/sidd/events");
-//     }
-// })
+        res.redirect("/sidd/events");
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const ejs = require("ejs");
 // app.use("/category" , router);
 
 const app = express();
+//Sidd
+const eventRouter = require("./routes/event");
+app.use("/" , eventRouter);
 
 app.use(express.static("node_modules/bootstrap/dist/css"));
 app.use(morgan('tiny'));
@@ -21,7 +24,6 @@ app.set("view engine", "html");
 const print = console.log;
 const VIEWS_PATH = path.join(__dirname, "/views/");
 
-let eventsDB = [];
 let categoriesDB = [];
 
 const PORT_NUMBER = 8080;
@@ -114,75 +116,8 @@ app.post("/category/33306036/delete-by-ID", function (req,res) {
     res.redirect("/category/33306036/list-all");
 });
 
-//Sidd
-const Event = require("./models/event");
 
-// const router = require("./routes/event-category");
-// app.use("/category" , router);
 
-//Adding a new event
-app.get("/sidd/events/add", function(req, res){
-    if (categoriesDB.length == 0){
-        res.sendFile( VIEWS_PATH + "add-event-without-category.html" );
-    } else{
-        res.sendFile( VIEWS_PATH + "add-event.html" );
-    }
-})
-
-app.post("/sidd/events/add", function(req, res){
-    let reqBody = req.body;
-    let newEvent = new Event(reqBody.name, reqBody.description, reqBody.startDateTime, reqBody.duration, reqBody.isActive, reqBody.image, reqBody.capacity, reqBody.ticketsAvailable, reqBody.categoryID);
-    eventsDB.push(newEvent);
-    res.redirect("/sidd/events");
-})
-
-//Listing all events
-app.get("/sidd/events", function(req, res){
-    res.render("list-all-events", { events: eventsDB });
-})
-
-// Listing sold out events
-app.get("/sidd/soldout-events", function(req, res){
-    res.render("list-soldout-events", { events: eventsDB });
-})
-
-// Show category detail
-app.get("/sidd/category", function(req, res){
-    const showCategoryId = req.query.id;
-    
-    if(categoriesDB.length == 0){
-        res.sendFile( VIEWS_PATH + "show-category-without-categories.html" );
-    }else{
-        if(showCategoryId==undefined){
-            res.render("show-category-details", { categories: categoriesDB, index: 0 , events: eventsDB});
-        }else{
-            for (let i = 0; i < categoriesDB.length; i++) {
-                if (categoriesDB[i].id == showCategoryId) {
-                    res.render("show-category-details", { categories: categoriesDB, index: i , events: eventsDB});
-                    break;
-                } 
-            }
-        }
-    }
-});
-
-//Deleting an event
-app.get("/sidd/events/delete", function(req, res){
-    const deleteid = req.query.id;
-    
-    if(deleteid==undefined){
-        res.sendFile( VIEWS_PATH + "delete-event.html" )
-    }else{
-        for (let i = 0; i < eventsDB.length; i++) {
-            if (eventsDB[i].id === deleteid) {
-                eventsDB.splice(i, 1);
-                break;
-            } 
-        }
-
-        res.redirect("/sidd/events");
-    }
-})
 
 //404 errors
 app.get("*", function(req, res) {
