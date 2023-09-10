c54498c02efe7fc549cb28a769db3316081d09de
sjad0005 <sjad0005@student.monash.edu>
2023-09-10 15:06:59 +1000

    Added endpoint routing

--- routes/event-api.js
+++ routes/event-api.js
@@ -1,64 +1,11 @@
-const Event = require("../models/event");
-const Category = require("../models/category");
+const express = require("express");
+const eventController = require("../controller/event-controller");
 
-module.exports = {
-	insertEvent: async function (req, res) {
-		let anEvent = new Event({ name: req.body.name, description: req.body.description, startTime: req.body.startTime, duration: req.body.duration, capacity: req.body.capacity, categories: req.body.categories});
-        
-        let categoryIDList = anEvent.categories.split(",");
-        categoryIDList.forEach(categoryID => {
-            let category = Category.findOne({categoryId: categoryID});
-            anEvent.categoryList.push(category._id);
-        });
-        
-		await anEvent.save();
-		res.json(anEvent.eventId);
-	},
+const router = express.Router();
 
-    listEvents: async function (req, res) {
-		let events = await Event.find({}).populate("category");
-        res.json(events);
-    },
+router.post("/add-event", eventController.insertEvent);
+router.get("/events", eventController.listEvents);
+router.put("/update-event", eventController.updateEvent);
+router.delete("/delete-event", eventController.deleteEvent);
 
-    updateEvent: async function (req, res) {
-		let eventID = req.body.eventId;
-        let name = req.body.name;
-        let capacity = req.body.capacity;
-
-        let anEvent = Event.findOne({eventId: eventID});
-        anEvent.name = name;
-        anEvent.capacity = capacity;
-        anEvent.save()
-
-        await anEvent.save();
-
-		res.status(200).json({
-            "status": "updated successfully"
-        })
-    },
-
-	deleteEvent: async function (req, res) {
-		let eventID = req.body.eventId;
-
-	    let anEvent = Event.findByIdAndRemove({eventId: eventID})
-
-        // This is the part where you delete the event from the category
-        // anEvent.categoryList.forEach(categoryID => {
-        //     let aCategory = Category.findOne({ _id: categoryID});
-             
-        //     for (let i=0; aCategory.events.length; i++){
-        //         if (aCategory.events[i] == ){
-        //             farm.animals.splice(i,1);
-        //             break;
-        //         }
-        //     }
-        // });
-
-        await anEvent.save();
-
-        res.status(200).json({
-            "acknowledged": true,
-            "deletedCount": 1
-        })
-	},
-};
+module.exports = router;
--- server.js
+++ server.js
@@ -1,38 +1,13 @@
-/**
- * @requires express
- */
+
 const mongoose = require("mongoose");
 const express = require('express');
-// const mongodb = require("mongodb");
 
-const url = "mongodb://127.0.0.1:27017/category";
-// async function 
+const url = "mongodb://127.0.0.1:27017/assignment02";
 
-
-/**
- * @requires morgan
- */
-const morgan = require("morgan")
-
-/**
- * @requires ejs
- */
-const ejs = require("ejs");
 const app = express();
 
-app.use(morgan('tiny'));
-
-/**
- * @requires body-parser
- */
-const bodyParser = require('body-parser');
-
-//Sidd
-/**
- * @requires event (router)
- */
-const eventRouter = require("./routes/event");
-app.use("/" , eventRouter);
+const eventRouter = require("./routes/event-api");
+app.use("/sidd/api/v1" , eventRouter);
 
 //Jade
 const categoryRouter = require("./routes/event-category");
@@ -50,56 +25,6 @@ connect(url)
 	.then(console.log)
 	.catch((err) => console.log(err));
 
-//bootstrap css files from node_modules
-app.use(express.static("node_modules/bootstrap/dist/css"));
-
-// Parse request bodies as JSON and URL-encoded
-app.use(bodyParser.urlencoded({ extended : true}));
-app.use(express.json());
-
-app.use(express.static("Images"));
-app.engine("html", ejs.renderFile);
-app.set("view engine", "html");
-
-const print = console.log;
-
-/**
- * The port number the server will listen on.
- * @type {number}
- * @constant
- */
-const PORT_NUMBER = 8080;
-/**
- * Start the server and listen on port 8080.
- * @name listen
- * @function
- * @param {number} port - port number
- * @param {Function} callback - callback function when the server starts.
- */
-app.listen(PORT_NUMBER, function () {
-	print(`listening on port ${PORT_NUMBER}`);
-});  
-
-/**
- * Default route handler for rendering the index view.
- * @name GET-/
- * @function
- * @param {string} path -express path
- * @param {Function} callback - Express callback
- * @param {import("express").Request} -request object
- * @param {import("express").Response} -response object
- */
-app.get("/" , function (req, res) {
-    res.render("index");
-});
-
-/**
- * Handling 404 errors.
- * @name GET-*
- * @function
- * @param {import("express").Request} -response object
- * @param {import("express").Response} -response object
- */
 app.get("*", function(req, res) {
     res.status(404).render("404.html");
 });
